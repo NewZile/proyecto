9b6211eae32b62789bd0005b103d7c1fc432cb3b
NewZile <105273978+NewZile@users.noreply.github.com>
2023-01-08 15:28:19 +0100

    Add files via upload

--- Login/registrar.php
+++ Login/registrar.php
@@ -1,29 +1,42 @@
 <?php
+$host = "localhost";
+$username = "root";
+$passwd = "";
+$dbname = "proyecto";
 
-if(!empty($_POST)){
-	if(isset($_POST["username"]) &&isset($_POST["fullname"]) &&isset($_POST["email"]) &&isset($_POST["password"]) &&isset($_POST["confirm_password"])){
-		if($_POST["username"]!=""&& $_POST["fullname"]!=""&&$_POST["email"]!=""&&$_POST["password"]!=""&&$_POST["password"]==$_POST["confirm_password"]){
-			include "conectar.php";
-			
-			$found=false;
-			$sql1= "select * from user where username=\"$_POST[username]\" or email=\"$_POST[email]\"";
-			$query = $con->query($sql1);
-			while ($r=$query->fetch_array()) {
-				$found=true;
-				break;
-			}
-			if($found){
-				print "<script>alert(\"Nombre de usuario o email ya estan registrados.\");window.location='../registro.php';</script>";
-			}
-			$sql = "insert into user(username,fullname,email,password,created_at) value (\"$_POST[username]\",\"$_POST[fullname]\",\"$_POST[email]\",\"$_POST[password]\",NOW())";
-			$query = $con->query($sql);
-			if($query!=null){
-				print "<script>alert(\"Registro exitoso. Proceda a logearse\");window.location='../login.php';</script>";
-			}
-		}
-	}
+//Creating a connection
+$con = mysqli_connect($host, $username, $passwd, $dbname);
+
+if ($con) {
+	print("Conexión Correctamente");
+} else {
+	print("Connection Failed ");
 }
 
 
+
+// Recogemos la información del formulario
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+// Validamos la información
+if (empty($username)) {
+	// El nombre de usuario está vacío, mostramos un mensaje de error
+	echo 'Por favor ingresa un nombre de usuario';
+} else if (empty($email)) {
+	// La dirección de correo electrónico está vacía, mostramos un mensaje de error
+	echo 'Por favor ingresa una dirección de correo electrónico';
+} else if (empty($password)) {
+	// La contraseña está vacía, mostramos un mensaje de error
+	echo 'Por favor ingresa una contraseña';
+} else {
+	// La información es válida, procedemos a verificar si el usuario y la dirección de correo electrónico ya existen
+	// (omitimos este paso en el ejemplo por simplicidad)
+
+	// Si el usuario y la dirección de correo electrónico son válidos, procedemos a registrar al usuario
+	// (omitimos este paso en el ejemplo por simplicidad)
+	echo 'Usuario registrado correctamente';
+}
 
 ?>
